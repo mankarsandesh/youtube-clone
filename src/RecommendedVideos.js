00a8f14a381226ca6f8ee6918d234a8087113c51
Sandesh <mankarsandesh11@gmail.com>
2020-07-31 17:09:20 +0700

    Working on Side Bar

---
+++ src/RecommendedVideos.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import  './RecommendedVideos.css';
+function RecommendedVideos() {
+    return (
+        <div className="RecommendedVideos">
+            i am RecommendedVideos
+        </div>
+    )
+}
+export default RecommendedVideos
