adc1418948b14d3c59793d78b72383ecfe4a3559
Sandesh <mankarsandesh11@gmail.com>
2020-07-31 11:19:52 +0700

    Import Project Setup

---
+++ firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBBw5rfJpD7sJnf1HsQRY-fqwpyL_e_5DU",
+    authDomain: "clone-95b09.firebaseapp.com",
+    databaseURL: "https://clone-95b09.firebaseio.com",
+    projectId: "clone-95b09",
+    storageBucket: "clone-95b09.appspot.com",
+    messagingSenderId: "675688166657",
+    appId: "1:675688166657:web:321d4c78a29659afb59700",
+    measurementId: "G-QL5P1DL8HY"
+  };
