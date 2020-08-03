102c7f8b144482e5d7aa61a546c45dac6debb01c
Sandesh <mankarsandesh11@gmail.com>
2020-08-03 18:14:57 +0700

    data add

--- src/RecommendedVideos.js
+++ src/RecommendedVideos.js
@@ -31,6 +31,54 @@ function RecommendedVideos() {
             channel="Sandesh Mankar"
             image="https://i.ytimg.com/vi/XzJ1WnFTVds/maxresdefault.jpg"
             />
+            <VideoCard 
+            title="My Name is sandesh"
+            views="2.3m views"
+            timestamp="3 days ago"
+            channelImage="https://i.ytimg.com/an_webp/1_IYL9ZMR_Y/mqdefault_6s.webp?du=3000&sqp=CPSAlfkF&rs=AOn4CLC-9l2Mjh_3SjaGhM2F7v_ihOy3cQ"
+            channel="Sandesh Mankar"
+            image="https://i.ytimg.com/vi/XzJ1WnFTVds/maxresdefault.jpg"
+            />
+            <VideoCard 
+            title="My Name is sandesh"
+            views="2.3m views"
+            timestamp="3 days ago"
+            channelImage="https://i.ytimg.com/an_webp/1_IYL9ZMR_Y/mqdefault_6s.webp?du=3000&sqp=CPSAlfkF&rs=AOn4CLC-9l2Mjh_3SjaGhM2F7v_ihOy3cQ"
+            channel="Sandesh Mankar"
+            image="https://i.ytimg.com/vi/XzJ1WnFTVds/maxresdefault.jpg"
+            />
+            <VideoCard 
+            title="My Name is sandesh"
+            views="2.3m views"
+            timestamp="3 days ago"
+            channelImage="https://i.ytimg.com/an_webp/1_IYL9ZMR_Y/mqdefault_6s.webp?du=3000&sqp=CPSAlfkF&rs=AOn4CLC-9l2Mjh_3SjaGhM2F7v_ihOy3cQ"
+            channel="Sandesh Mankar"
+            image="https://i.ytimg.com/vi/XzJ1WnFTVds/maxresdefault.jpg"
+            />
+             <VideoCard 
+            title="My Name is sandesh"
+            views="2.3m views"
+            timestamp="3 days ago"
+            channelImage="https://i.ytimg.com/an_webp/1_IYL9ZMR_Y/mqdefault_6s.webp?du=3000&sqp=CPSAlfkF&rs=AOn4CLC-9l2Mjh_3SjaGhM2F7v_ihOy3cQ"
+            channel="Sandesh Mankar"
+            image="https://i.ytimg.com/vi/XzJ1WnFTVds/maxresdefault.jpg"
+            />
+            <VideoCard 
+            title="My Name is sandesh"
+            views="2.3m views"
+            timestamp="3 days ago"
+            channelImage="https://i.ytimg.com/an_webp/1_IYL9ZMR_Y/mqdefault_6s.webp?du=3000&sqp=CPSAlfkF&rs=AOn4CLC-9l2Mjh_3SjaGhM2F7v_ihOy3cQ"
+            channel="Sandesh Mankar"
+            image="https://i.ytimg.com/vi/XzJ1WnFTVds/maxresdefault.jpg"
+            />
+            <VideoCard 
+            title="My Name is sandesh"
+            views="2.3m views"
+            timestamp="3 days ago"
+            channelImage="https://i.ytimg.com/an_webp/1_IYL9ZMR_Y/mqdefault_6s.webp?du=3000&sqp=CPSAlfkF&rs=AOn4CLC-9l2Mjh_3SjaGhM2F7v_ihOy3cQ"
+            channel="Sandesh Mankar"
+            image="https://i.ytimg.com/vi/XzJ1WnFTVds/maxresdefault.jpg"
+            />
           </div>
         </div>
     )
