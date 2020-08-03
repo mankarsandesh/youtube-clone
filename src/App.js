b38ae0393c8b43a302bdb9c1abcc1437c9746017
Sandesh <mankarsandesh11@gmail.com>
2020-08-03 18:06:55 +0700

    youtube Search page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import React from 'react';
 import './App.css';
-import Header from  "./Header";
+import Header from "./Header";
 import Sidebar from "./Sidebar";
 import RecommendedVideos from "./RecommendedVideos";
-
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import SearchPage from "./SearchPage";
 function App() {
   return (
-    <div className="App">   
-      <Header/>
-      <div className="app__page">
-        <Sidebar />
-        <RecommendedVideos />
+    <div className="App">
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/search">
+          <div className="app__page">
+              <Sidebar />
+              <SearchPage />
+            </div>
 
-      </div>
+          </Route>
+          <Route path="/">
+
+            <div className="app__page">
+              <Sidebar />
+              <RecommendedVideos />
+            </div>
+          </Route>
+
+        </Switch>
+      </Router>
+
 
     </div>
   );
--- src/Header.js
+++ src/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css';
 import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
@@ -6,18 +6,25 @@ import VideoCallIcon from '@material-ui/icons/VideoCall';
 import AppsIcon from '@material-ui/icons/Apps';
 import NotificationsIcon from '@material-ui/icons/Notifications';
 import Avatar from '@material-ui/core/Avatar';
+import { Link } from 'react-router-dom';
 
 function Header() {
+    const [inputSearch, setInputSearch] = useState("");
     return (
         <div className="header">
             <div className="header__left">
                 <MenuIcon />
-                <img className="header__logo"
-                    src="https://www.youtube.com/about/static/svgs/icons/brand-resources/YouTube-logo-full_color_light.svg?cache=72a5d9c" />
+                <Link to="/">
+                    <img className="header__logo"
+                        src="https://www.youtube.com/about/static/svgs/icons/brand-resources/YouTube-logo-full_color_light.svg?cache=72a5d9c" />
+                </Link>
+
             </div>
             <div className="header__input">
-                <input type="text" placeholder="Search" />
-                <SearchIcon className="header__searchInputButton"/>
+                <input type="text" onChange={(e) => setInputSearch(e.target.value)} value={inputSearch} placeholder="Search" />
+                <Link to={`search/${inputSearch}`}>
+                    <SearchIcon className="header__searchInputButton" />
+                </Link>
             </div>
             <div className="header__icons">
                 <VideoCallIcon className="header__icon" />
